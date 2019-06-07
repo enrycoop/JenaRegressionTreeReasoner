1573a165806b921aac80037c4c4e90cfa7bcdfa4
enric <enric@172.26.111.129>
2019-06-07 17:36:17 +0200

    SPARQL query

--- SemanticWebTechnologies/src/main/java/MainClass.java
+++ SemanticWebTechnologies/src/main/java/MainClass.java
@@ -1,68 +1,51 @@
-import java.io.PrintWriter;
-import org.apache.jena.query.Query;
+import java.util.LinkedList;
 import org.apache.jena.query.QueryExecution;
-import org.apache.jena.query.QueryExecutionFactory;
-import org.apache.jena.query.QueryFactory;
+import org.apache.jena.query.QuerySolution;
 import org.apache.jena.query.ResultSet;
-import org.apache.jena.query.ResultSetFormatter;
-import org.apache.jena.rdf.model.Model;
-import org.apache.jena.rdf.model.ModelFactory;
-import org.apache.jena.rdf.model.NodeIterator;
-import org.apache.jena.rdf.model.Property;
-import org.apache.jena.rdf.model.RDFNode;
-import org.apache.jena.rdf.model.Resource;
-import org.apache.jena.rdf.model.Statement;
-import org.apache.jena.rdf.model.StmtIterator;
 import org.apache.jena.rdfconnection.RDFConnection;
 import org.apache.jena.rdfconnection.RDFConnectionFactory;
-import org.apache.jena.vocabulary.OWL;
-import org.apache.jena.vocabulary.RDFS;
-
-
 /**
  *
- * @author enrico
+ * @author enrico coppolecchia
  */
 public class MainClass {
-
+    
     public static void main(String[] args) {
-        String sURI = "http://dbpedia.org/page/Virtuoso_Universal_Server/";
-        
-        try {
-            // Creazione di un grafo vuoto
-            Model model = ModelFactory.createDefaultModel();
-            model.read(sURI);
-            Resource res = model.getResource("http://www.semanticweb.org/ontologies/2018/9/untitled-ontology-53#Body_Temp");
-            StmtIterator iter = res.listProperties();
-            while(iter.hasNext()){
-                Statement stmt = iter.next();
-                Resource r = stmt.getSubject();
-                System.err.print(r.getLocalName());
-                
-                Property p = stmt.getPredicate();
-                System.err.print(" ---- " + p.getLocalName());
-                
-                RDFNode node = stmt.getObject();
-                
-                System.err.println(" ----> " + node.toString());
-            }
-            String s = ""
-                    + "\nSELECT ?a ?c"
-                    + "\nWHERE{?a rdf:type ?c.}";
-            //------------------------------------------------------QUERY
-            Query query = QueryFactory.create(s);
-            // Esecuzione della query e cattura dei risultati
-            QueryExecution qe = QueryExecutionFactory.create(query, model);
-            ResultSet results = qe.execSelect();
-            // Stampa dei risultati su schermo
-            ResultSetFormatter.out(System.out, results, query);
-            // Chiusura della struttura dati per il rilascio di memoria
-            qe.close();
-
-            } catch (Exception e) {
-                System.out.println("Failed: " + e);
-            }
+        try (RDFConnection conn = RDFConnectionFactory.connect("http://dbpedia.org/sparql/")) {
+           
+            QueryExecution qExec = conn.query("PREFIX dbpedia: <http://dbpedia.org/ontology/>\n"
+                    + "   SELECT DISTINCT ?mag ?apoapsis ?escape ?rotPer\n"
+                    + "   WHERE{\n"
+                    + "      ?CelestialBody a dbpedia:Planet.\n"
+                    + "      ?CelestialBody dbpedia:absoluteMagnitude ?mag.\n"
+                    + "      ?CelestialBody dbpedia:apoapsis ?apoapsis.\n"
+                    + "      ?CelestialBody dbpedia:escapeVelocity ?escape.\n"
+                    + "      ?CelestialBody dbpedia:rotationPeriod ?rotPer.\n"
+                    + "   } ");
+            ResultSet rs = qExec.execSelect();
             
+            LinkedList<LinkedList<Double>> dataset = new LinkedList<LinkedList<Double>>();
+            while (rs.hasNext()) {
+                QuerySolution qs = rs.next();
+                LinkedList<Double> touple = new LinkedList<Double>();
+                touple.add(qs.getLiteral("apoapsis").getDouble());
+                touple.add(qs.getLiteral("escape").getDouble());
+                touple.add(qs.getLiteral("rotPer").getDouble());
+                touple.add(qs.getLiteral("mag").getDouble());
+                dataset.add(touple);
+            }
+            System.out.println(dataset);
+            qExec.close();
         }
-        
     }
+    
+    public static Double[][] parseToArray(LinkedList<LinkedList<Double>> dataset){
+        Double[][] array = new Double[dataset.size()][dataset.get(0).size()];
+        for(int i = 0 ; i<dataset.size();i++ )
+            for(int j =0 ; j< dataset.get(0).size();j++){
+                array[i][j] = dataset.get(i).get(j);
+            }
+        return array;
+    }
+
+}
