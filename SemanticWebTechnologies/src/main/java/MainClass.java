6494744037d3d8be98d0a5a9a6a9e8a6f3e661d0
enric <enric@172.26.111.129>
2019-06-12 15:19:26 +0200

    jena reasoner updated. Some function converted in python.

--- SemanticWebTechnologies/src/main/java/MainClass.java
+++ SemanticWebTechnologies/src/main/java/MainClass.java
@@ -33,52 +33,12 @@ import org.apache.jena.util.PrintUtil;
 public class MainClass {
 
     public static void main(String[] args) throws IOException {
-
-        try (RDFConnection conn = RDFConnectionFactory.connect("http://dbpedia.org/sparql/")) {
-           
-            QueryExecution qExec = conn.query("PREFIX dbpedia: <http://dbpedia.org/ontology/>\n" +
-"   SELECT DISTINCT ?CelestialBody ?mag ?apoapsis ?periapsis ?escape ?rotPer ?orbitalPer ?albedo ?target\n" +
-"   WHERE{\n" +
-"      ?CelestialBody a dbpedia:Planet.\n" +
-"      ?CelestialBody dbpedia:absoluteMagnitude ?mag.\n" +
-"      ?CelestialBody dbpedia:apoapsis ?apoapsis.\n" +
-"      ?CelestialBody dbpedia:escapeVelocity ?escape.\n" +
-"      ?CelestialBody dbpedia:rotationPeriod ?rotPer.\n" +
-"      ?CelestialBody dbpedia:orbitalPeriod ?orbitalPer.\n" +
-"      ?CelestialBody dbpedia:periapsis ?periapsis.\n" +
-"      ?CelestialBody dbpedia:albedo ?albedo.\n" +
-"      OPTIONAL{\n" +
-"       ?CelestialBody dbpedia:temperature ?target.\n" +
-"      }\n"
-                    + "FILTER(!bound(?target))\n" +
-"      \n" +
-"   } ");
-            ResultSet rs = qExec.execSelect();
-            
-            BufferedWriter
-            sc = new BufferedWriter(new FileWriter("C:\\Users\\enric\\Desktop\\python-works\\SemanticWeb\\Planets.n3"));
-            
-            while (rs.hasNext()) {
-                QuerySolution qs = rs.next();
-                sc.write("<"+qs.getResource("CelestialBody")+"> <https://cs.dbpedia.org/ontology/Planet/apoapsis> "+qs.getLiteral("apoapsis").getString()+".\n");
-                sc.write("<"+qs.getResource("CelestialBody")+"> <https://cs.dbpedia.org/ontology/Planet/absoluteMagnitude> "+qs.getLiteral("mag").getString()+".\n");
-                sc.write("<"+qs.getResource("CelestialBody")+"> <https://cs.dbpedia.org/ontology/Planet/periapsis> "+qs.getLiteral("periapsis").getString()+".\n");
-                sc.write("<"+qs.getResource("CelestialBody")+"> <https://cs.dbpedia.org/ontology/Planet/escapeVelocity> "+qs.getLiteral("escape").getString()+".\n");
-                sc.write("<"+qs.getResource("CelestialBody")+"> <https://cs.dbpedia.org/ontology/Planet/rotationPeriod> "+qs.getLiteral("rotPer").getString()+".\n");
-                sc.write("<"+qs.getResource("CelestialBody")+"> <https://cs.dbpedia.org/ontology/Planet/orbitalPeriod> "+qs.getLiteral("orbitalPer").getString()+".\n");
-                sc.write("<"+qs.getResource("CelestialBody")+"> <https://cs.dbpedia.org/ontology/Planet/albedo> "+qs.getLiteral("albedo").getString()+".\n");
-            }
-            qExec.close();
-            sc.close();
-        }
         Model data = FileManager.get().loadModel("C:\\Users\\enric\\Desktop\\python-works\\SemanticWeb\\Planets.n3");
         List rules = Rule.rulesFromURL("C:\\Users\\enric\\Desktop\\python-works\\SemanticWeb\\rules_result.rules");
 
         GenericRuleReasoner reasoner = new GenericRuleReasoner(rules);
         
         InfModel inf = ModelFactory.createInfModel(reasoner, data);
-        
-        
         
         StmtIterator i = inf.listStatements();
         while (i.hasNext()) {
